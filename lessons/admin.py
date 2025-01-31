d1f9dc0d04bb10263589811cb0bf27679f60f227
Matt Miles <matt_miles2@live.co.uk>
2025-01-31 12:59:03 +0000

    Add User comments and comment counter.

--- lessons/admin.py
+++ lessons/admin.py
@@ -30,4 +30,4 @@ class CommentAdmin(admin.ModelAdmin):
     """
     Admin interface for the Comment model.
     """
-    list_display = ('lesson', 'commenter', 'body', 'created_on')
+    list_display = ('lesson', 'commenter', 'body', 'created_on', 'comment_status')
--- lessons/views.py
+++ lessons/views.py
@@ -3,6 +3,8 @@ from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Subject, Lesson, Comment
+from .forms import CommentForm
+
 
 
 def home(request):
@@ -52,4 +54,28 @@ def lesson_detail(request, slug):
     
     queryset = Lesson.objects.filter(lesson_status=1)
     lesson = get_object_or_404(queryset, slug=slug)
-    return render(request, "lessons/lesson_detail.html", {"lesson": lesson})
+    comments = lesson.comments.all().order_by("created_on")
+    comment_count = lesson.comments.filter(comment_status=0).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.commenter = request.user
+            comment.lesson = lesson
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS, 
+                "Comment submitted successfully!")
+    
+    comment_form = CommentForm()
+
+    return render(
+        request, "lessons/lesson_detail.html", 
+        {
+            "lesson": lesson,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
+    )
