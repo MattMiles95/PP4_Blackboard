36872331c847245d06fe646c86f3b3a0ccb4f07d
Matt Miles <matt_miles2@live.co.uk>
2025-01-31 20:02:01 +0000

    Add ability for Users to edit & delete comments

--- lessons/urls.py
+++ lessons/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('lessons/hist', views.HistoryLessons.as_view(), name='hist'),
     path('lessons/psych', views.PsychologyLessons.as_view(), name='psych'),
     path('<slug:slug>/', views.lesson_detail, name='lesson_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
     ]
--- lessons/views.py
+++ lessons/views.py
@@ -79,3 +79,42 @@ def lesson_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    Edit an individual comment.
+    """
+    if request.method == "POST":
+
+        queryset = Lesson.objects.filter(lesson_status=1)
+        lesson = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.commenter == request.user:
+            comment = comment_form.save(commit=False)
+            comment.lesson = lesson
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('lesson_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    Delete an individual comment.
+    """
+    queryset = Lesson.objects.filter(lesson_status=1)
+    lesson = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+    
+    if comment.commenter == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('lesson_detail', args=[slug]))
