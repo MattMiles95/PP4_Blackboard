03d59e9b948baeaf1fcb3b24a8f074b338c9d895
Matt Miles <matt_miles2@live.co.uk>
2025-02-05 11:25:51 +0000

    Fix database error (delete and replace database with new, empty database). Add homework submission model. Update all migrations.

--- homework/admin.py
+++ homework/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Homework
 
-# Register your models here.
+
+@admin.register(Homework)
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ('student', 'lesson', 'submitted_at', 'status')
+    list_filter = ('status', 'submitted_at')
+    search_fields = ('student__username', 'lesson__title', 'notes')
+    readonly_fields = ('submitted_at',)
+    ordering = ['-submitted_at']
--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from lessons.models import Lesson, Subject
 
-# Create your models here.
+class Homework(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, )
+    file = models.FileField(upload_to='homework_files/')
+    notes = models.TextField(blank=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=15,
+        choices=[
+            ('pending', 'Pending Review'),
+            ('marked', 'Marked'),
+        ],
+        default='pending'
+    )
+
+    def __str__(self):
+        return f"Homework: {self.lesson.title} by {self.student.username}"
+
+    class Meta:
+        ordering = ['-submitted_at']
